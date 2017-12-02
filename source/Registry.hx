52cffeaed805ae99255471f7f400e58a2c9f11d7
clue <colin.h.lu@gmail.com>
2017-12-02 15:11:50 -0600

    added another easy level

--- source/Registry.hx
+++ source/Registry.hx
@@ -2,10 +2,12 @@ package;
 
 class Registry
 {
-    public static var levelList:Array<String> = [AssetPaths.long_icy_floor__tmx,
-                            AssetPaths.tutorial_level_one__tmx,
-                            AssetPaths.test_level__tmx, 
-                            AssetPaths.one_jump__tmx,
-                            AssetPaths.big_cave__tmx];
+    public static var levelList:Array<String> = [
+        AssetPaths.long_icy_floor__tmx,
+        AssetPaths.jumping_test__tmx,
+        AssetPaths.tutorial_level_one__tmx,
+        AssetPaths.test_level__tmx, 
+        AssetPaths.one_jump__tmx,
+        AssetPaths.big_cave__tmx];
     public static var currLevel:Int = 0; 
 }
