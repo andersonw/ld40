fd194d7e27a9e0cf6c0b27b092a8026522179d23
clue <colin.h.lu@gmail.com>
2017-12-02 13:40:57 -0600

    add new level

--- source/Registry.hx
+++ source/Registry.hx
@@ -4,6 +4,7 @@ class Registry
 {
     public static var levelList:Array<String> = [AssetPaths.tutorial_level_one__tmx,
                             AssetPaths.test_level__tmx, 
-                            AssetPaths.one_jump__tmx];
+                            AssetPaths.one_jump__tmx,
+                            AssetPaths.big_cave__tmx];
     public static var currLevel:Int = 0; 
 }
