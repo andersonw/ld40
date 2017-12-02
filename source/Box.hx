4cf942d55ec860af1c413c95f99c3f8e774e6452
Jon Schneider <jschneider2013@gmail.com>
2017-12-02 14:49:09 -0500

    actual box skeleton

---
+++ source/Box.hx
@@ -0,0 +1,85 @@
+package;
+
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+import Constants.BoxPhysics.*;
+
+class Box extends Wall
+{
+    public var carrier:FlxSprite;
+    public var carried:Bool = false;
+    public var carrierOffset:FlxPoint;
+
+    public var onFloor:Bool = false;
+    public var onIce:Bool = false;
+
+    public function new(?X:Float=0, ?Y:Float=0, ?width:Int=32, ?height:Int=32)
+    {
+        super(X, Y, width, height);
+        makeGraphic(width, height, new FlxColor(0xffb0cccc));
+        immovable=true;
+    }
+
+    public function getCarried(carrier:FlxSprite){
+        this.carrier = carrier;
+        carrierOffset = new FlxPoint(x - carrier.x, y - carrier.y);
+        
+        carried = true;
+    }
+
+    public function getDropped(){
+        this.carrier = null;
+        carried = false;
+    }
+
+    public override function update(elapsed:Float):Void
+    {
+        movement();
+
+        super.update(elapsed);
+    }
+
+    private function movement():Void
+    {
+        if(!carried){
+            // horizontal stuff
+            var vX:Float = velocity.x;
+            var vY:Float = velocity.y;
+            vY = vY + GRAVITY;
+
+            // apply drag
+            if (onFloor) {
+                // no drag if holding left or right or if on ice
+                if(!onIce){
+                    vX *= FLOOR_DRAG;    
+                }
+            } else {
+                vX *= HORIZONTAL_DRAG;
+            }
+            vY *= VERTICAL_DRAG;
+
+            // terminal speeds
+            // (for the most part air resistance should take care of this) 
+            if(vX > HORIZONTAL_TERMINAL_SPEED){
+                vX = HORIZONTAL_TERMINAL_SPEED;
+            }else if(vX < -HORIZONTAL_TERMINAL_SPEED){
+                vX = -HORIZONTAL_TERMINAL_SPEED;
+            }
+
+            if(vY > VERTICAL_TERMINAL_SPEED){
+                vY = VERTICAL_TERMINAL_SPEED;
+            }else if(vY < -VERTICAL_TERMINAL_SPEED){
+                vY = -VERTICAL_TERMINAL_SPEED;
+            }
+
+            if (vX < 0.01 && vX > -0.01) vX = 0;
+            velocity.set(vX, vY);
+        }else{
+            x = carrier.x + carrierOffset.x;
+            y = carrier.y + carrierOffset.y;
+        }
+        
+    }
+}
---
+++ source/Constants.hx
@@ -0,0 +1,30 @@
+package;
+
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class PlayerPhysics{
+    public static inline var GRAVITY:Float = 10;
+
+    public static inline var HORIZONTAL_TERMINAL_SPEED:Float = 200;
+    public static inline var VERTICAL_TERMINAL_SPEED:Float = 400;
+
+    public static inline var VERTICAL_DRAG:Float = 0.99;
+    public static inline var HORIZONTAL_DRAG:Float = 0.98;
+    public static inline var FLOOR_DRAG:Float = 0.85;
+
+    public static inline var HORIZONTAL_ACCELERATION:Float = 50;
+    public static inline var JUMP_SPEED:Float = 400;
+    public static inline var DASH_LENGTH:Float = 150;
+}
+
+class BoxPhysics{
+    public static inline var GRAVITY:Float = 10;
+
+    public static inline var HORIZONTAL_TERMINAL_SPEED:Float = 200;
+    public static inline var VERTICAL_TERMINAL_SPEED:Float = 400;
+    
+    public static inline var VERTICAL_DRAG:Float = 0.99;
+    public static inline var HORIZONTAL_DRAG:Float = 0.98;
+    public static inline var FLOOR_DRAG:Float = 0.85;
+}
