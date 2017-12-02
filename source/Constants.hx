b9acf11c5452e4a35427c11d27f455c8b37b93f3
Anderson Wang <anderson.wang1994@gmail.com>
2017-12-02 15:22:41 -0500

    change some constants, add new level

--- source/Constants.hx
+++ source/Constants.hx
@@ -7,14 +7,14 @@ class PlayerPhysics{
     public static inline var GRAVITY:Float = 10;
 
     public static inline var HORIZONTAL_TERMINAL_SPEED:Float = 200;
-    public static inline var VERTICAL_TERMINAL_SPEED:Float = 400;
+    public static inline var VERTICAL_TERMINAL_SPEED:Float = 600;
 
     public static inline var VERTICAL_DRAG:Float = 0.99;
-    public static inline var HORIZONTAL_DRAG:Float = 0.98;
+    public static inline var HORIZONTAL_DRAG:Float = 1.00;
     public static inline var FLOOR_DRAG:Float = 0.85;
 
     public static inline var HORIZONTAL_ACCELERATION:Float = 50;
-    public static inline var JUMP_SPEED:Float = 400;
+    public static inline var JUMP_SPEED:Float = 600;
     public static inline var DASH_LENGTH:Float = 150;
 }
 
