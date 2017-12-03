e2c9eabfa4a941422aa3ab8fbf0df6eb097a1f5d
pzai <alaraen@gmail.com>
2017-12-03 00:52:15 -0500

    reordered levels

--- source/Registry.hx
+++ source/Registry.hx
@@ -3,14 +3,15 @@ package;
 class Registry
 {
     public static var levelList:Array<String> = [
-                            AssetPaths.boxy_advanced__tmx,
-                            AssetPaths.boxy__tmx,
-                            AssetPaths.long_icy_floor__tmx,
-                            AssetPaths.jumping_test__tmx,
-                            AssetPaths.tutorial_level_one__tmx,
-                            AssetPaths.tutorial_level_two__tmx, 
-                            AssetPaths.one_jump__tmx,
-                            AssetPaths.big_cave__tmx];
+        AssetPaths.boxy__tmx,
+        AssetPaths.tutorial_level_one__tmx,
+        AssetPaths.jumping_test__tmx,
+        AssetPaths.one_jump__tmx,
+        AssetPaths.tutorial_level_two__tmx, 
+        AssetPaths.big_cave__tmx,
+        AssetPaths.long_icy_floor__tmx,
+        AssetPaths.boxy_advanced__tmx
+    ];
 
     public static var currLevel:Int = 0; 
 }
