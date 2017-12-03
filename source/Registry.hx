418f86d1b86e23b80a68ba8983e382c0aa6fe4b4
clue <colin.h.lu@gmail.com>
2017-12-03 17:01:52 -0600

    tutorial level updates

--- source/Registry.hx
+++ source/Registry.hx
@@ -11,11 +11,11 @@ class Registry
         AssetPaths.basic_movement__tmx,
         AssetPaths.tutorial_level_one__tmx,
         AssetPaths.jumping_test__tmx,
-        AssetPaths.box_intro_level__tmx,
         AssetPaths.two_jumps__tmx,
         AssetPaths.tutorial_level_two__tmx, 
         AssetPaths.big_cave__tmx,
         AssetPaths.long_icy_floor__tmx,
+        AssetPaths.box_intro_level__tmx,
         AssetPaths.boxy_advanced__tmx
     ];
 
