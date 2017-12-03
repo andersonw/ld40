d2c290831e8093e4f58caba7a626a2291d91a255
Jon Schneider <jschneider2013@gmail.com>
2017-12-02 23:43:11 -0500

    whoops boxes are not ice

--- source/Box.hx
+++ source/Box.hx
@@ -18,7 +18,6 @@ class Box extends Wall
     public function new(?X:Float=0, ?Y:Float=0, ?width:Int=32, ?height:Int=32)
     {
         super(X, Y, width, height);
-        wallType = Wall.WallType.ICE;
         loadGraphic(AssetPaths.cardboard_box__png);
         immovable=false;
     }
